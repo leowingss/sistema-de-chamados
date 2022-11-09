c00678ac4d5eca2dc5a307d8302aec529182937d
Leonardo <leondani853@gmail.com>
2022-11-09 11:35:02 -0300

    feat: pagina dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { AuthContext } from '../../contexts/auth';
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiMessageSquare, FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import './dashboard.css';
 
 export default function Dashboard() {
+
+    const [chamados, setChamados] = useState([]);
 
     const { signOut } = useContext(AuthContext);
 
@@ -14,8 +20,33 @@ export default function Dashboard() {
     return (
         <div>
             <Header />
-            <h1>Dashboard</h1>
-            <button onClick={handleSignOut}>Sair</button>
+            <div className='content'>
+                <Title name='Atendimentos'>
+                    <FiMessageSquare color='#000' size={25} />
+                </Title>
+
+                {chamados.length === 0 ? (
+                    <div className='container dashboard'>
+                        <span>Nenhum chamado registrado</span>
+                        <Link to='/new' className='new'>
+                            <FiPlus size={25} color='#fff' />
+                            Novo chamado
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        <Link to='/new' className='new'>
+                            <FiPlus size={25} color='#fff' />
+                            Novo chamado
+                        </Link>
+                    </>
+                )
+                }
+
+
+
+            </div>
+
         </div>
     );
 }
